493ce50d0bf7e8e13758388899b8a05a20b36ac1
takecchi <takecchi.kobayashi@gmail.com>
2026-02-27 18:19:23 +0900

    card

--- packages/ui/.storybook/main.ts
+++ packages/ui/.storybook/main.ts
@@ -8,6 +8,7 @@ const config: StorybookConfig = {
     "@chromatic-com/storybook",
     "@storybook/addon-interactions",
   ],
+  staticDirs: [{ from: './static', to: '/static' }],
   framework: {
     name: "@storybook/react-vite",
     options: {},
