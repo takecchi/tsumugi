fb7c27bf7b2ca443946a4e61fad599cf7a7784a9
takecchi <takecchi.kobayashi@gmail.com>
2026-02-27 19:00:28 +0900

    iconの設定

--- apps/desktop/app/root.tsx
+++ apps/desktop/app/root.tsx
@@ -6,6 +6,11 @@ import { SafeErrorBoundary, SafeMeta, SafeLinks } from '@tsumugi/react-router';
 import './app.css';
 
 export const links: LinksFunction = () => [
+  { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+  { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+  { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+  { rel: 'icon', type: 'image/png', sizes: '192x192', href: '/icon-192.png' },
+  { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
   { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
   {
     rel: 'preconnect',
